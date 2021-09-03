210624dafc4fe31dca07d3e0acbb6f9f55c1b21e
Karine <karine.samyn@hogent.be>
2021-09-03 07:10:47 +0200

    add state, starrating and places component

--- src/mock-data.js
+++ src/mock-data.js
@@ -11,12 +11,27 @@ const TRANSACTION_DATA = [
 	  date: '2021-06-30T10:09:22.534Z',
 	  place: 'Loon',
 	},
-  ];
-  
-  const PLACE_DATA = [
-	{ id: '7f28c5f9-d711-4cd6-ac15-d13d71abff83', name: 'Loon', rating: 5 },
-	{ id: '7f28c5f9-d711-4cd6-ac15-d13d71abff84', name: 'Dranken Geers', rating: 3 },
-	{ id: '7f28c5f9-d711-4cd6-ac15-d13d71abff85', name: 'Irish Pub', rating: 4 }
-  ];
-  
-  export {TRANSACTION_DATA, PLACE_DATA};
+];
+
+const PLACE_DATA = [
+  {
+    id: '7f28c5f9-d711-4cd6-ac15-d13d71abff83',
+    name: 'Loon',
+    rating: 5,
+  },
+  {
+    id: '7f28c5f9-d711-4cd6-ac15-d13d71abff84',
+    name: 'Dranken Geers',
+    rating: 3,
+  },
+  {
+    id: '7f28c5f9-d711-4cd6-ac15-d13d71abff85',
+    name: 'Irish Pub',
+    rating: 4,
+  }
+];
+
+export {
+  TRANSACTION_DATA,
+  PLACE_DATA,
+};
