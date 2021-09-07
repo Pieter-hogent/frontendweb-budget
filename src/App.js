f5a0b9aaa9c7e005f4aa41a85e3f0d586f781645
Karine <karine.samyn@hogent.be>
2021-09-07 23:47:28 +0200

    Fetch the data from the backend

--- src/App.js
+++ src/App.js
@@ -12,35 +12,35 @@ import Transactions from "./pages/Transactions";
 import { PlacesProvider } from "./contexts/PlacesProvider";
 
 function App() {
-    return (
-      <PlacesProvider>
-        <TransactionsProvider>
-          <Router>
-            <Switch>
-              <Route path="/" exact>
-                <Redirect to="/transactions" />
-              </Route>
+  return (
+    <PlacesProvider>
+      <TransactionsProvider>
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/transactions" />
+            </Route>
 
-              <Route path="/transactions" exact>
-                <Transactions />
-              </Route>
+            <Route path="/transactions" exact>
+              <Transactions />
+            </Route>
 
-              <Route path="/transactions/add" exact>
-                <TransactionForm />
-              </Route>
+            <Route path="/transactions/add" exact>
+              <TransactionForm />
+            </Route>
 
-              <Route path="/transactions/edit/:id" exact>
-                <TransactionForm />
-              </Route>
+            <Route path="/transactions/edit/:id" exact>
+              <TransactionForm />
+            </Route>
 
-              <Route path="/places">
-                <Places />
-              </Route>
-            </Switch>
-          </Router>
-        </TransactionsProvider>
-      </PlacesProvider>
-    );
+            <Route path="/places">
+              <Places />
+            </Route>
+          </Switch>
+        </Router>
+      </TransactionsProvider>
+    </PlacesProvider>
+  );
 }
 
 export default App;
